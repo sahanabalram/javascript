1955d2931846a47bdb807b0cb8f2e5ef74bb2211
Sahana Balram <sahana2008balram@gmail.com>
2017-07-18 16:04:24 -0700

    added on 18th

---
+++ sum.js
@@ -0,0 +1,6 @@
+ function sum(firstNumber, secondNumber) {
+     var mySum = firstNumber + secondNumber;
+     return 'The sum of ' + firstNumber + ' and ' + secondNumber + ' equals ' + mySum;
+ }
+
+ console.log(sum(2, 4));
