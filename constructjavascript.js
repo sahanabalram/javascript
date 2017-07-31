ad0a5a667428fc66a7b8abfa9b724bf3a1fafd0b
Sahana Balram <sahana2008balram@gmail.com>
2017-07-31 15:36:14 -0700

    constructed a new instance of an Object

---
+++ constructjavascript.js
@@ -0,0 +1,15 @@
+
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 5;
+};
+
+// Only change code below this line.
+
+var MotorBike = function() {
+  this.wheels = 2;
+  this.engines = 1;
+  this.seats = 1;
+};
+console.log(motorBike);
